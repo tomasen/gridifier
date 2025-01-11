007c28c7e8d0314431b75f0855423f6672394e6b
Tomasen <tomasen@gmail.com>
2025-01-11 07:05:49 +0000

    Update package dependencies and improve layout in Home component
    - Added @types/lodash as a development dependency in package.json and package-lock.json.
    - Modified the layout of the Home component in page.tsx for better responsiveness, adjusting flex properties and margins.

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -71,13 +71,12 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 p-8">
-
       <main className="max-w-7xl mx-auto gap-8">
         <section className="p-6 mb-8 space-y-4 w-full">
           <div className="space-y-4">
-            <h1 className="text-4xl font-bold flex items-baseline bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+            <h1 className="text-4xl font-bold flex flex-col md:flex-row items-baseline bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
               Gridifier
-              <p className="text-sm ml-4 flex text-gray-600 dark:text-gray-300">
+              <p className="text-sm md:ml-4 flex mt-2 md:mt-0 text-gray-600 dark:text-gray-300">
                 Customizable grid system based on your own 3D models.
               </p>
             </h1>
